c71479741cbc28ebf8582a68786e217b9212f01a
Kronosfear <vijayrajasekar1@gmail.com>
2017-04-02 01:51:49 +0530

    fucc

--- Beanbot.py
+++ Beanbot.py
@@ -59,7 +59,57 @@ async def on_message(message):
             else:
                 reply_message = reply_message + ' LUL'
             await client.send_message(message.channel, reply_message)
+            
+        elif message.content.startswith('!weeb'):
+            reply_message = "WEEB VoHiYo THE VoHiYo NORMIES VoHiYo AWAY"
+            await client.send_message(message.channel, reply_message)
 
+        elif message.content.startswith('!koi'):
+            reply_message = 'https://www.youtube.com/watch?v=gK57X6WWi5E'
+            await client.send_message(message.channel, reply_message)
+            
+        elif message.content.startswith('!wutface'):
+            reply_message = "https://cdn.discordapp.com/attachments/259440947434225664/297810508650905611/16998849_993717407439407_6365115539161661315_n.jpg"
+            await client.send_message(message.channel, reply_message)
+
+        elif message.content.startswith('!love'):
+            pride = message.content[5:]
+            if pride == '':
+                pride = 'Windows 10 updates'
+            mmr = randint(0,100)
+            reply_message = 'Hey ' + str(message.author).split('#', 1)[0]
+            reply_message = reply_message + ', your love for '
+            reply_message = reply_message +  pride
+            reply_message = reply_message + ' is around '
+            reply_message = reply_message + str(mmr)
+            if(mmr > 50):
+                reply_message = reply_message + '%'
+                if pride == 'Windows 10 updates':
+                     reply_message = reply_message + ' because you didn\'t mention any name'
+                     reply_message = reply_message + ' LUL'
+                else:
+                    reply_message = reply_message + ' KappaPride'
+            else:
+                reply_message = reply_message + '%'
+                if pride == 'Windows 10 updates':
+                    reply_message = reply_message + ' because you didn\'t mention any name'
+                    reply_message = reply_message + ' LUL'
+                else:
+                    reply_message = reply_message + ' FeelsBadMan'
+            await client.send_message(message.channel, reply_message)
+
+        elif message.content.startswith('!uniok'):
+            reply_message = 'http://i.imgur.com/HdxYRqM.png'
+            await client.send_message(message.channel, reply_message)
+
+        elif message.content.startswith('!matuslap'):
+            reply_message = 'https://gfycat.com/SlightVastCottonmouth'
+            await client.send_message(message.channel, reply_message)
+
+        elif message.content.startswith('!fuccboi'):
+            reply_message = 'sheep is today\'s fuccboi'
+            await client.send_message(message.channel, reply_message)
+            
             
         elif message.content.startswith('!mydong'):
             mmr = randint(0,25)
@@ -67,6 +117,21 @@ async def on_message(message):
             reply_message = reply_message + ', your dong hangs '
             reply_message = reply_message + str(mmr)
             reply_message = reply_message + ' cms low Jebaited'
+            await client.send_message(message.channel, reply_message)
+
+        elif message.content.startswith('!help'):
+            reply_message = """
+            ```
+            
+Here are a list of commands Beanchild can reply to:
+!info - Check whether Singsing is live or not.
+!mymmr - Check your true MMR.
+!mydong - ur mum knows about it anyway haHAA
+!love - KappaPride
+!weeb - the normies away
+!bean - FRICCIN
+!wutface - weebs
+        ```"""
             await client.send_message(message.channel, reply_message)
 
             
