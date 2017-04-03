79a37b9475a60f523aafaca62c1f7858e2a4bc56
Kronosfear <vijayrajasekar1@gmail.com>
2017-04-03 08:17:14 +0530

    gsearch

--- Beanbot.py
+++ Beanbot.py
@@ -6,10 +6,13 @@ from random import randint
 import argparse
 import requests
 import json
-
-
+import random
+from google import google
 
 client = discord.Client()
+
+#*************************************************************************************************************************
+
 
 
 async def my_background_task():
@@ -25,7 +28,11 @@ async def my_background_task():
         else:
             reply_message = 'Master Sing is live'
         await client.change_presence(game=discord.Game(name=reply_message))
-        await asyncio.sleep(600) # task runs every 60 seconds
+        await asyncio.sleep(60) # task runs every 60 seconds
+
+
+#*************************************************************************************************************************
+
 
 @client.event
 async def on_ready():
@@ -33,6 +40,12 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
+
+
+    
+#*************************************************************************************************************************
+
+
 
 @client.event
 async def on_message(message):
@@ -49,6 +62,7 @@ async def on_message(message):
                 print(reply_message)
             await client.send_message(message.channel, reply_message)
 
+ #-----------------------------------------------------------------------------------------------------------
             
         elif message.content.startswith('!mymmr'):
             mmr = randint(0,9999)
@@ -59,18 +73,31 @@ async def on_message(message):
             else:
                 reply_message = reply_message + ' LUL'
             await client.send_message(message.channel, reply_message)
+
+            
+ #-----------------------------------------------------------------------------------------------------------
+
             
         elif message.content.startswith('!weeb'):
             reply_message = "WEEB VoHiYo THE VoHiYo NORMIES VoHiYo AWAY"
             await client.send_message(message.channel, reply_message)
 
+
+ #-----------------------------------------------------------------------------------------------------------
+
+
         elif message.content.startswith('!koi'):
             reply_message = 'https://www.youtube.com/watch?v=gK57X6WWi5E'
             await client.send_message(message.channel, reply_message)
+
+ #-----------------------------------------------------------------------------------------------------------
+
             
         elif message.content.startswith('!wutface'):
             reply_message = "https://cdn.discordapp.com/attachments/259440947434225664/297810508650905611/16998849_993717407439407_6365115539161661315_n.jpg"
             await client.send_message(message.channel, reply_message)
+
+ #-----------------------------------------------------------------------------------------------------------
 
         elif message.content.startswith('!love'):
             pride = message.content[5:]
@@ -98,19 +125,65 @@ async def on_message(message):
                     reply_message = reply_message + ' FeelsBadMan'
             await client.send_message(message.channel, reply_message)
 
+            
+ #-----------------------------------------------------------------------------------------------------------
+
         elif message.content.startswith('!uniok'):
             reply_message = 'http://i.imgur.com/HdxYRqM.png'
             await client.send_message(message.channel, reply_message)
+
+ #-----------------------------------------------------------------------------------------------------------            
 
         elif message.content.startswith('!matuslap'):
             reply_message = 'https://gfycat.com/SlightVastCottonmouth'
             await client.send_message(message.channel, reply_message)
 
+ #-----------------------------------------------------------------------------------------------------------
+
+
+        elif message.content.startswith('!grill'):
+            grill_list = ['https://b.catgirlsare.sexy/aXbo.png',
+                                      'https://cdn.discordapp.com/attachments/259440947434225664/298268447479955458/matu.jpg',
+                                      'https://i.imgur.com/uVVvtEm.jpg',
+                                      'https://pbs.twimg.com/media/CgP1ps8WEAAMuMp.jpg',
+                                      'https://instagram.fmaa1-1.fna.fbcdn.net/t51.2885-15/e35/14488244_558745484310394_67739065410335',
+                                      'https://pbs.twimg.com/media/CLkmgJ6WsAApMxz.jpg'
+                                    ]
+            random.shuffle(grill_list)
+            reply_message = grill_list[0]
+            await client.send_message(message.channel, reply_message)
+
+
+ #-----------------------------------------------------------------------------------------------------------
+
+        elif message.content.startswith('!gsearch'):
+            query = message.content[9:]
+            search_results = google.search(query, 1)
+            reply_message = "Hey "
+            reply_message = reply_message + str(message.author).split('#', 1)[0]
+            reply_message = reply_message + ", here are the top results for your search: **" + query
+            reply_message = reply_message + "**\n\n"
+            for i in range(0,3):
+                j = i + 1
+                reply_message = reply_message + str(j)
+                reply_message = reply_message + ". "
+                reply_message = reply_message + search_results[i].name
+                reply_message = reply_message + " - <"
+                reply_message = reply_message + search_results[i].link
+                reply_message = reply_message + ">\n"
+                reply_message = reply_message + search_results[i].description
+                reply_message = reply_message + "\n\n"
+            await client.send_message(message.channel, reply_message)
+
+ #-----------------------------------------------------------------------------------------------------------
+            
         elif message.content.startswith('!fuccboi'):
             reply_message = 'sheep is today\'s fuccboi'
             await client.send_message(message.channel, reply_message)
             
-            
+#-----------------------------------------------------------------------------------------------------------
+
+
         elif message.content.startswith('!mydong'):
             mmr = randint(0,25)
             reply_message = 'Hey ' + str(message.author).split('#', 1)[0]
@@ -118,6 +191,8 @@ async def on_message(message):
             reply_message = reply_message + str(mmr)
             reply_message = reply_message + ' cms low Jebaited'
             await client.send_message(message.channel, reply_message)
+
+ #-----------------------------------------------------------------------------------------------------------
 
         elif message.content.startswith('!help'):
             reply_message = """
@@ -131,10 +206,14 @@ Here are a list of commands Beanchild can reply to:
 !weeb - the normies away
 !bean - FRICCIN
 !wutface - weebs
+!grill - Kreygasm
+!matuslap - dat ass
         ```"""
             await client.send_message(message.channel, reply_message)
 
-            
+ #-----------------------------------------------------------------------------------------------------------
+
+ 
         elif message.content.startswith('!bean'):
             await client.send_message(message.channel, "http://i0.kym-cdn.com/photos/images/facebook/001/166/993/284.png")
         
